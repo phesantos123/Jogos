258d1205d5bfb32906c0496d229e454c6ac4eda1
Pedro Henrique <phsantos021@gmail.com>
2022-04-17 20:30:06 -0300

    creat tab

--- Memoria/assets/scripts/script.js
+++ Memoria/assets/scripts/script.js
@@ -14,7 +14,27 @@ let techs = [
   'react'
 ]
 
-createCardsFromTechs(techs)
+let cards = null
+startGame()
+function startGame() {
+  cards = createCardsFromTechs(techs)
+  shuffleCards(cards)
+  console.log(cards)
+}
+
+function shuffleCards(cards) {
+  let currentIndex = cards.length
+  let randomIndex = 0
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[cards[randomIndex], cards[currentIndex]] = [
+      cards[currentIndex],
+      cards[randomIndex]
+    ]
+  }
+}
 
 function createCardsFromTechs(techs) {
   let cards = []
